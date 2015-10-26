ec73fc4c5e397351dbea55cf57d233ca29e9f7c7
Soichiro-Yoshimura <soichiro_yoshimura@dwango.co.jp>
2015-10-26 13:10:21 +0900

    comment out hooks

--- index.js
+++ index.js
@@ -23,47 +23,22 @@ module.exports = {
         }
     },
 
-    // Extend ebook resources and html
-    website: {
-        assets: "./book",
-        js: [
-            "test.js"
-        ],
-        css: [
-            "test.css"
-        ],
-        html: {
-            "html:start": function() {
-                return "<!-- Start book "+this.options.title+" -->"
-            },
-            "html:end": function() {
-                return "<!-- End of book "+this.options.title+" -->"
-            },
-
-            "head:start": "<!-- head:start -->",
-            "head:end": "<!-- head:end -->",
-
-            "body:start": "<!-- body:start -->",
-            "body:end": "<!-- body:end -->"
-        }
-    },
-
     // Extend templating blocks
     blocks: {
         // Author will be able to write "{% myTag %}World{% endMyTag %}"
-        myTag: {
-            process: function(blk) {
-                return "Hello "+blk.body;
-            }
-        }
+        // myTag: {
+        //     process: function(blk) {
+        //         return "Hello "+blk.body;
+        //     }
+        // }
     },
 
     // Extend templating filters
     filters: {
         // Author will be able to write "{{ 'test'|myFilter }}"
-        myFilter: function(s) {
-            return "Hello "+s;
-        }
+        // myFilter: function(s) {
+        //     return "Hello "+s;
+        // }
     },
 
     // Hook process during build
@@ -72,12 +47,10 @@ module.exports = {
 
         // This is called before the book is generated
         "init": function() {
-            console.log("init!");
         },
 
         // This is called after the book generation
         "finish": function() {
-            console.log("finish!");
         }
     }
 };
